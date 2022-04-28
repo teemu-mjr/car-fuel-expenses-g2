c6b594a6c23988b1b27ad9b98895cb657da3ed58
Teemu Rintala <771847375+teemu-mjr@users.noreply.github.com>
2022-04-29 00:24:13 +0300

    styles FuelingExpense

--- src/components/FuelingExpense.js
+++ src/components/FuelingExpense.js
@@ -1,52 +1,74 @@
 import React, { useContext } from "react";
+import { Card, Col, Container, Button, Row } from "react-bootstrap";
 import { GlobalContext } from "../context/GlobalState";
 
 export const FuelingExpense = ({ fuelingExpenses, byName = false }) => {
   const { deleteExpense, deleteExpenseByName } = useContext(GlobalContext);
 
   return (
-    <div className="card bg-dark m-3">
-      <div className="d-flex justify-content-around card-header">
+    <Card border="light" bg="dark" className="m-3">
+      <Card.Header>
         <p className="text-info">{fuelingExpenses.carName}</p>
-      </div>
-      <div className="card-body">
-        <div className="d-flex justify-content-between">
-          <p>Price: </p>
-          <p className="text-info">{fuelingExpenses.price}</p>
-        </div>
-        <div className="d-flex justify-content-between">
-          <p>Kilometers: </p>
-          <p className="text-info">{fuelingExpenses.kilometer}</p>
-        </div>
-        <div className="d-flex justify-content-between">
-          <p>Liters: </p>
-          <p className="text-info">{fuelingExpenses.liter}</p>
-        </div>
-        <div className="d-flex justify-content-between">
-          <p>Km/L: </p>
-          <p className="text-info">
-            {(fuelingExpenses.kilometer / fuelingExpenses.liter).toFixed(2)}
-          </p>
-        </div>
-        <div className="d-flex justify-content-between">
-          <p>Price/Km: </p>
-          <p className="text-info">
-            {(fuelingExpenses.price / fuelingExpenses.kilometer).toFixed(4)}
-          </p>
-        </div>
-        <button
-          className="btn btn-danger"
-          onClick={() => {
-            if (!byName) {
-              deleteExpense(fuelingExpenses.id);
-            } else {
-              deleteExpenseByName(fuelingExpenses.carName);
-            }
-          }}
-        >
-          Delete
-        </button>
-      </div>
-    </div>
+      </Card.Header>
+      <Card.Body>
+        <Container>
+          <Row>
+            <Col>
+              <p>Price: </p>
+            </Col>
+            <Col>
+              <p className="text-info">{fuelingExpenses.price}</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+            <p>Kilometers: </p>
+            </Col>
+            <Col>
+            <p className="text-info">{fuelingExpenses.kilometer}</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+            <p>Liters: </p>
+            </Col>
+            <Col>
+            <p className="text-info">{fuelingExpenses.liter}</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+            <p>Km/L: </p>
+            </Col>
+            <Col>
+            <p className="text-info">
+              {(fuelingExpenses.kilometer / fuelingExpenses.liter).toFixed(2)}
+            </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+            <p>Price/Km: </p>
+            </Col>
+            <Col>
+            <p className="text-info">
+              {(fuelingExpenses.price / fuelingExpenses.kilometer).toFixed(4)}
+            </p>
+            </Col>
+          </Row>
+          <Button variant="outline-danger"
+            onClick={() => {
+              if (!byName) {
+                deleteExpense(fuelingExpenses.id);
+              } else {
+                deleteExpenseByName(fuelingExpenses.carName);
+              }
+            }}
+          >
+            Delete
+          </Button>
+        </Container>
+      </Card.Body>
+    </Card>
   );
 };
--- src/components/FuelingLog.js
+++ src/components/FuelingLog.js
@@ -1,15 +1,16 @@
 import React, { useContext } from "react";
 import { FuelingExpense } from "./FuelingExpense";
 import { GlobalContext } from "../context/GlobalState";
+import { Card, Container } from "react-bootstrap";
 
 export const FuelingLog = () => {
   const { fuelingExpenses } = useContext(GlobalContext);
 
   return (
-    <div className="d-flex flex-column align-items-center text-center">
+    <Container className="align-items-center text-center">
       <h1>Fueling expenses log</h1>
       {fuelingExpenses.length > 0 ? (
-        <div style={{ minWidth: "200px", maxWidth: "500px", width: "50vw" }}>
+        <div>
           {fuelingExpenses.map((fuelingExpenses) => (
             <FuelingExpense
               key={fuelingExpenses.id}
@@ -18,12 +19,12 @@ export const FuelingLog = () => {
           ))}
         </div>
       ) : (
-        <div className="card bg-dark m-3">
-          <div className="card-body">
+        <Card bg="dark">
+          <Card.Body>
             <p>This is where all of you expenses are logged idividually.</p>
-          </div>
-        </div>
+          </Card.Body>
+        </Card>
       )}
-    </div>
+    </Container>
   );
 };
